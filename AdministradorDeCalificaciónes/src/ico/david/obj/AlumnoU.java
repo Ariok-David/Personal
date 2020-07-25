880a69a8992c70642b9f13df9d19fb5fb1ec7c83
Ariok-David <ariokleon368@aragon.unam.mx>
2020-07-24 20:04:33 -0500

    Añadi las variables necesarias al objeto AlumnoU

---
+++ AdministradorDeCalificaciónes/src/ico/david/obj/AlumnoU.java
@@ -0,0 +1,14 @@
+package ico.david.obj;
+
+/**
+ *
+ * @author Ariok David
+ */
+public class AlumnoU {
+    private int cuentaUNAM;
+    private String nombre;
+    private String apellidos;
+    private String institucion;
+    private String carrera;
+    private int semestre;    
+}
