2af5bdb9aee5300c9991f46cfb2b45d23460c448
Ariok-David <ariokleon368@aragon.unam.mx>
2020-07-24 20:50:40 -0500

    termine el objeto AlumnoU

--- AdministradorDeCalificaciónes/src/ico/david/obj/AlumnoU.java
+++ AdministradorDeCalificaciónes/src/ico/david/obj/AlumnoU.java
@@ -10,5 +10,56 @@ public class AlumnoU {
     private String apellidos;
     private String institucion;
     private String carrera;
-    private int semestre;    
+
+    public AlumnoU() {
+    }
+
+    public AlumnoU(int cuentaUNAM, String nombre, String apellidos, String institucion, String carrera) {
+        this.cuentaUNAM = cuentaUNAM;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.institucion = institucion;
+        this.carrera = carrera;
+    }
+
+    public int getCuentaUNAM() {
+        return cuentaUNAM;
+    }
+
+    public void setCuentaUNAM(int cuentaUNAM) {
+        this.cuentaUNAM = cuentaUNAM;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getInstitucion() {
+        return institucion;
+    }
+
+    public void setInstitucion(String institucion) {
+        this.institucion = institucion;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+    
 }
